a4a71ce33369cc53128e695e44106ee1eee418fb
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-11 11:51:06 +0530

    thread using  the class

---
+++ Javaa/src/threaddd/Main.java
@@ -0,0 +1,25 @@
+package threaddd;
+
+
+class ThreadClass extends Thread {
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+
+		ThreadClass t1 = new ThreadClass();
+		t1.start();
+	}
+
+}
