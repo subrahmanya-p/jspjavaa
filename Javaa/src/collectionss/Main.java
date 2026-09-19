1c8e1fcbd171c342b590f91ebe7bffa257325dc9
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-19 23:21:17 +0530

    done

--- Javaa/src/collectionss/Main.java
+++ Javaa/src/collectionss/Main.java
@@ -1,21 +1,49 @@
 package collectionss;
 
+import java.util.Arrays;
+
 public class Main {
-	static Object[] arr = new Object[10];
 
-	public static int size() {
-		int count = 0;
-		for (Object i : arr) {
-			if (i != null)
-				count++;
+    static Object[] arr = new Object[10];
 
-		}
-		return count;
+    static int size = 0;
 
-	}
+    static int size() {
+        return size;
+    }
 
-	public static void main(String[] args) {
-		arr[0] = 60;
-	}
+    static void add(Object obj) {
+        arr[size] = obj;
+        size++;
+    }
+public static void add(Object obj ,int index) {
+	System.arraycopy(arr, index, arr, index+1, size-index);
+	arr[index]=obj;
+	size++;
+	
+}
+    public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(arr));
+
+        add(60);
+        add(8);
+        add(6);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Size: " + size());
+
+        add(20, 1);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Size: " + size());
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Size: " + size());
+
+        add(10, 4);
+      
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
