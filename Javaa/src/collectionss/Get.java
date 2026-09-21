4a4331f36b8b17c014964e1d19ae31568fdf20ce
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-21 11:01:29 +0530

    done

---
+++ Javaa/src/collectionss/Get.java
@@ -0,0 +1,17 @@
+package collectionss;
+
+public class Get {
+	static Object[] srcArr = { 2, 3, 4, 6, 0, 8 };
+
+	static Object get(int index) {
+		return srcArr[index];
+	}
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("heloo");
+System.out.println(get(3));
+
+	}
+
+}
