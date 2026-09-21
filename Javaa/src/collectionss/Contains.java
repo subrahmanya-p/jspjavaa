9f50cf26269bfca0eb72d71a89fa22f54e48d07a
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-21 22:20:33 +0530

    done

---
+++ Javaa/src/collectionss/Contains.java
@@ -0,0 +1,22 @@
+package collectionss;
+
+public class Contains {
+	static Object[] arr = { 10, 34, 55, 43, 12, 13, 14, 15 };
+
+	static boolean contains(Object obj) {
+		for (Object element : arr) {
+			if (element != null && element.equals(obj)) {
+				return true;
+			}
+
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(contains(34));
+		// TODO Auto-generated method stub
+
+	}
+
+}
